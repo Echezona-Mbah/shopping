b2b524e40ff67f16c66617d02a8a5f1c20b14799
Echezona-Mbah <cclassico998@gmail.com>
2024-05-21 09:32:03 -0700

    Update of debug error

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\CartItem;
+use App\Models\Watch;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function showUserCount()
+    {
+        $CartItemCount = CartItem::count();
+
+        $watch = Watch::all();
+       // dd($userCount);die();
+
+        return view('admin.home', compact('CartItemCount','watch'));
+    }
 }
