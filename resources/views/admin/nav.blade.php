d42d4c6bd7c6148fdb9f89f0daaf130e31368692
Echezona-Mbah <cclassico998@gmail.com>
2024-05-22 00:24:54 -0700

    Update of debug error

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -16,55 +16,29 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="{{ route('watch-table') }}">
-          <i class="typcn typcn-user-add-outline menu-icon"></i>
-          <span class="menu-title">All Product</span>
+            <i class="typcn typcn-user-add-outline menu-icon"></i>
+            <span class="menu-title">All Product</span>
         </a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-          <i class="typcn typcn-user-add-outline menu-icon"></i>
-          <span class="menu-title">User Pages</span>
-          <i class="menu-arrow"></i>
+    </li>
+    
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('cart-table') }}">
+            <i class="typcn typcn-globe-outline menu-icon"></i>
+            <span class="menu-title">All Cart</span>
         </a>
-        <div class="collapse" id="auth">
-          <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-          </ul>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
-          <i class="typcn typcn-globe-outline menu-icon"></i>
-          <span class="menu-title">Error pages</span>
-          <i class="menu-arrow"></i>
-        </a>
-        <div class="collapse" id="error">
-          <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
-          </ul>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://bootstrapdash.com/demo/polluxui-free/docs/documentation.html">
-          <i class="typcn typcn-mortar-board menu-icon"></i>
-          <span class="menu-title">Documentation</span>
-        </a>
-      </li>
-
-      <li class="nav-item">
+    </li>
+    
+    <li class="nav-item">
         <form method="POST" action="{{ route('logout') }}">
-          @csrf
-
-          <x-dropdown-link :href="route('logout')"
-                  onclick="event.preventDefault();
-                              this.closest('form').submit();">
-              {{ __('Log Out') }}
-          </x-dropdown-link>
-      </form>
-      </li>
+            @csrf
+            <a class="nav-link" href="{{ route('logout') }}"
+               onclick="event.preventDefault(); this.closest('form').submit();">
+                <i class="typcn typcn-mortar-board menu-icon"></i>
+                <span class="menu-title">Log Out</span>
+            </a>
+        </form>
+    </li>
+    
 
     </ul>
   </nav>                     
